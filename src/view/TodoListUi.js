ba89e5d07bbe1c7329bb20010511d2657c90cd7f
songyan <songyan@clyyw.cn>
2020-05-28 10:37:56 +0800

    无状态ui组件，提高性能

--- src/view/TodoListUi.js
+++ src/view/TodoListUi.js
@@ -1,29 +1,24 @@
-import React, {Component } from "react";
-import { Input,Button,List,  } from 'antd'
-class TodoList extends Component {
-    constructor(props) {
-        super(props)
-    }
-    render() { 
-        return ( 
+import React from "react";
+import { Input,Button,List } from 'antd'
+function TodoList(props) {
+    return ( 
+        <div>
             <div>
-                <div>
-                    <Input placeholder={'Write Something'} value={this.props.inputValue} onChange={this.props.chnageValue} style={{ width:'250px'}}/>
-                    <Button type="primary" onClick={this.props.addList}>增加</Button>
-                    <List
-                        style={{margin:'10px auto',width:'300px'}}
-                        bordered
-                        dataSource={this.props.list}
-                        renderItem={(item,index) => (
-                            <List.Item>
-                                {item}
-                                <Button size='small' type="primary" danger onClick={()=>this.props.deletelist(index)}>删除</Button>
-                            </List.Item>
-                        )}
-                    />
-                </div>
+                <Input placeholder={'Write Something'} value={props.inputValue} onChange={props.chnageValue} style={{ width:'250px'}}/>
+                <Button type="primary" onClick={props.addList}>增加</Button>
+                <List
+                    style={{margin:'10px auto',width:'300px'}}
+                    bordered
+                    dataSource={props.list}
+                    renderItem={(item,index) => (
+                        <List.Item>
+                            {item}
+                            <Button size='small' type="primary" danger onClick={()=>props.deletelist(index)}>删除</Button>
+                        </List.Item>
+                    )}
+                />
             </div>
-         );
-    }
+        </div>
+        )
 }
 export default TodoList
