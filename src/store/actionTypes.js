0803d142b87084ba76c6e495cfbe66f59c392ae6
songyan <songyan@clyyw.cn>
2020-05-29 09:18:38 +0800

    学习react-redux

--- src/store/actionTypes.js
+++
@@ -1,4 +0,0 @@
-export const  CHANGE_INPUT = 'changeInput'
-export const  ADD_ITEM = 'addlist'
-export const  DELETE_ITEM = 'dellist'
-export const  initList = 'initlist'
--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-// 引入redux里面的createStore方法
-import { createStore,applyMiddleware } from 'redux'
-// 使用thunk中间件
-import thunk from 'redux-thunk'
-import reducer from './reducer'
-// 创建一个store对象
-const store = createStore(reducer,applyMiddleware(thunk)) // 创建数据存储仓库
-export default store
